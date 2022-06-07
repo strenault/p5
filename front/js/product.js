ee3d8f5d23df0cf30f3d5968e89bee197cf87ad8
stephane Renault <strenault@wanadoo.fr>
2022-06-07 20:53:28 +0200

    le produit est arrivé sur la page 2

--- front/js/product.js
+++ front/js/product.js
@@ -1,55 +1,58 @@
 /**
  * Gère l'affichage et les interactions de la Product
  */
-fillmain();
-
 //récupératin de l'id kanap
-const queryString_url = window.location.search;
-const urlParams = new URLSearchParams(queryString_url);
-const id = urlParams.get('id');
+const produit = window.location.search.split("?id=").join("");
+console.log(produit);
+let produitData = [];
 
 // Récupération des articles de l'API
-async function getArticles() {
-    var articlesProduct = await fetch("http://localhost:3000/api/products$id")
+const fetchProduit = async () => {
+    await fetch(`http://localhost:3000/api/products/${produit}`)
+        .then((res) => res.json())
+        .then((promise) => {
+            produitData = promise;
+            console.log(produitData);
+        });
+};
+const produitItem = async () => {
+    await fetchProduit(); {
+            .then(function (ficheProduit) {
+        const article = ficheProduit;
+        console.table(articles);
+        for (let article in articles) {
+
+            // Insertion de l'image
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = ficheProduit[article].imageUrl;
+            productImg.alt = ficheProduit[article].altTxt;
 
 
+            // Insertion de l'élément "article"
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+
+
+            // Insertion du titre "h3"
+            let productName = document.createElement("h1");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = resultatAPI[article].name;
+
+            // Insertion de la description "p"
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(item__content__description__title);
+            item__content__description__title.classList.add("productName");
+            item__content__description__title.innerHTML = resultatAPI[article].description;
+        }
+    })
+    .catch((erreur) => console.log('Accès aux Produits impossible'))
+    };
 }
 
+produitItem();
+
+
 // Répartition des données de l'API dans le DOM
-async function fillmain() {
-    var result = await fetch("http://localhost:3000/api/products$id")
-        .then(function (idArticle) {
-            const articles = resultatAPI;
-            console.table(articles);
-            for (let article in articles) {
-
-                // Insertion de l'élément "a"
-                let productLink = document.createElement("a");
-                document.querySelector(".items").appendChild(productLink);
-                productLink.href = `product.html?id=${resultatAPI[article]._id}`;
-
-                // Insertion de l'élément "article"
-                let productArticle = document.createElement("article");
-                productLink.appendChild(productArticle);
-
-                // Insertion de l'image
-                let productImg = document.createElement("img");
-                productArticle.appendChild(productImg);
-                productImg.src = resultatAPI[article].imageUrl;
-                productImg.alt = resultatAPI[article].altTxt;
-
-                // Insertion du titre "h3"
-                let productName = document.createElement("h3");
-                productArticle.appendChild(productName);
-                productName.classList.add("productName");
-                productName.innerHTML = resultatAPI[article].name;
-
-                // Insertion de la description "p"
-                let productDescription = document.createElement("p");
-                productArticle.appendChild(productDescription);
-                productDescription.classList.add("productName");
-                productDescription.innerHTML = resultatAPI[article].description;
-            }
-        })
-        .catch((erreur) => console.log('Accès aux Produits impossible'))
-};
